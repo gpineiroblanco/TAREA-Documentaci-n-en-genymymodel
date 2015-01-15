3307268b1a20ade4a5e6666e352fa66b28189c6a
gpineiroblanco <gpineiroblanco@eniac14.danielcastelao.org>
2015-01-15 09:49:49 +0100

    Commit Debug fuera de tiempo.

---
+++ src/main/java/model/Adestrador.java
@@ -0,0 +1,64 @@
+package model;
+
+
+/**
+ * Contiene id de la federación de los adiestradores.<div>Pueden dirigir partidos y entrenar.</div>
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Adestrador
+{
+	/**
+	 * id de federacion tipo string
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String idFederacion;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public Persoal persoal;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Adestrador(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void DirixirPartido() {
+		// TODO implement me	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void Entrenar() {
+		// TODO implement me	
+	}
+	
+}
+
